cfc01ecf2a43c910e655503f632252afedc0d3b1
spurthiu <42243915+spurthiu@users.noreply.github.com>
2018-09-25 12:33:27 +0530

    Create questions.c

---
+++ questions.c
@@ -0,0 +1,21 @@
+1.
+    what is #include<stdio.h>
+ #define the preprocessor,includes the standard input and output functions inside it.
+2.
+    what is printf()?
+This function is used to print the character,string,float onthe output screen.
+3.
+    what is main?
+    It is a return type ,also called operating system used to run the program.
+4.
+    Why should you learn touch typing on a qwerty keyboard?
+It has been proven that a trained touch typist can save around 20 – 35% of their time used on a computer through learning this skill.
+Decrease Fatigue - Typing is both psychologically and physically exhausting when done for long periods of time.
+Learning to touch type properly reduces mental and physical fatigue.
+5.
+    How will you learn touch typing on a qwerty keyboard?
+QWERTY: If you haven't noticed yet all the keyboards will have a similar layout. The design is known as the QWERTY layout.
+It is an industry standard for keyboard design. Even your smartphone's touch key will use QWERTY.
+Place your left hand on a, s, d, f and right on j, k, l, ;.
+There are small little markers on keys F and L.
+Every keyboard will have it. The purpose of having those markers is to place your hands on them without looking at it.
