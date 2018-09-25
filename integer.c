f393946b33fe2684ea5e49c71d86df8b8b0dc29f
spurthiu <42243915+spurthiu@users.noreply.github.com>
2018-09-25 12:57:51 +0530

    Create integer.c

---
+++ integer.c
@@ -0,0 +1,36 @@
+1.
+#include <stdio.h>
+int main()
+{
+    int number;
+    printf("Enter an integer: ");  
+    scanf("%d", &number);  
+    printf("You entered: %d", number);
+    return 0;
+}
+
+2.
+What is #include <stdio.h>?
+# is the preprocessor, " include" gives command to include something, "stdio" stands for "standard input output"
+.h extension, It contains some standard functions input and output.
+ 
+ 3.
+ What is printf?
+  This function is used to print the character, string, float, integer onto the output screen.
+  
+  4.
+  What is main?
+  It is a return type used to run the program.
+  
+  5.
+  What is a variable?
+  naming of an address is known as variable.
+  Unlike constant, variables are changeable, we can change value of a variable during execution of a program. 
+  
+  6.
+  What is type of a variable?
+  int - integer: 
+  float - a decimal number.
+  double - a double-precision floating point value.
+  char - a single character.
+  
